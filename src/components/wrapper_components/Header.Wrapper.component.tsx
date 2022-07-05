57d35ebfb837e225c102d1ed08a30e3f98f23c9f
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-06 01:03:09 +0530

    Implement Header Wrapper Component

---
+++ src/components/wrapper_components/Header.Wrapper.component.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Box, IconButton } from "@mui/material";
+import Text from "./Text.wrapperComponent";
+import MoreIcon from "@mui/icons-material/MoreVert";
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#6133BD",
+      }}
+    >
+      <div style={styles.headerText}>
+        <Text variant="h6" color="#FFFFFF">
+          Push Notification Implementation
+        </Text>
+      </div>
+      <div>
+        <IconButton
+          size="large"
+          aria-label="display more actions"
+          edge="end"
+          color="inherit"
+        >
+          <MoreIcon sx={{ color: "#FFFFFF" }} />
+        </IconButton>
+      </div>
+    </Box>
+  );
+};
+
+const styles = {
+  headerContainer: {
+    width: "100%",
+    backgroundColor: "#6133BD",
+    height: "60px",
+    display: "flex",
+    alignItems: "center",
+  },
+  headerText: {
+    marginLeft: "15px",
+  },
+};
+
+export default Header;
