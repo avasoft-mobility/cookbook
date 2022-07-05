861537d9d3738b9563a252f1a819887e2de45ec2
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-05 22:26:23 +0530

    Implement-Search

--- src/components/Search.component.tsx
+++ src/components/Search.component.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Input from "./wrapper_components/Input.wrapper.component";
+import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = (props: SvgIconProps) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignSelf: "center",
+        borderRadius: "16px",
+        boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      <div style={styles.searchIconContainer}>
+        <SearchIcon />
+      </div>
+      <Input />
+    </div>
+  );
+};
+const styles = {
+  searchIconContainer: {
+    height: "40px",
+    display: "flex",
+    alignItems: "center",
+  },
 };
 
 export default Search;
---
+++ src/components/wrapper_components/Input.wrapper.component.tsx
@@ -0,0 +1,28 @@
+import { borderColor, borderRadius } from "@mui/system";
+import React from "react";
+
+const Input = () => {
+  return (
+    <div style={{ width: "97%" }}>
+      <input
+        style={styles.inputContainer}
+        type="text"
+        id="inputID"
+        placeholder="Explore the topics..."
+      />
+    </div>
+  );
+};
+const styles = {
+  inputContainer: {
+    borderRadius: "none",
+    borderWidth: "0px",
+    outline: "none",
+    width: "100%",
+    height: "50px",
+    display: "flex",
+    fontSize: "20px",
+  },
+};
+
+export default Input;
