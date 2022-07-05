9ab4f729d220a608cc82f035ab837a26ac5a82bd
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-05 18:52:27 +0530

    Implement text wrapper component
    Modify component file name from plural to singular

---
+++ src/components/wrapper_components/Text.wrapperComponent.tsx
@@ -0,0 +1,38 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+interface TextProps {
+  children: any;
+  gutterBottom?: boolean;
+  noWrap?: boolean;
+  paragraph?: boolean;
+  variant:
+    | "body1"
+    | "body2"
+    | "button"
+    | "caption"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "inherit"
+    | "overline"
+    | "subtitle1"
+    | "subtitle2";
+}
+const Text: React.FC<TextProps> = (props) => {
+  return (
+    <Typography
+      gutterBottom={props.gutterBottom}
+      noWrap={props.noWrap}
+      paragraph={props.paragraph}
+      variant={props.variant}
+    >
+      {props.children}
+    </Typography>
+  );
+};
+
+export default Text;
--- src/components/wrapper_components/Text.wrapperComponents.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Text = () => {
-  return <div>Text.wrapperComponents</div>;
-};
-
-export default Text;
