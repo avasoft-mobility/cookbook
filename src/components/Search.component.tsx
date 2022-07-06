d8fdf03a657f6e0f0bda23feaacaa2ebb5b06fb6
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-06 14:49:40 +0530

    Implement onchange for SearchComponent

--- src/components/Search.component.tsx
+++ src/components/Search.component.tsx
@@ -6,20 +6,23 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 
 const Search = (props: SvgIconProps) => {
+  const searchText = (text: string) => {
+    console.log(text);
+  };
   return (
     <Paper
-      component="form"
       sx={{
         display: "flex",
         alignItems: "center",
         boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.25)",
         borderRadius: "16px",
+        marginTop: "10px",
       }}
     >
       <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
         <SearchIcon color="primary" />
       </IconButton>
-      <Input />
+      <Input input={searchText} />
     </Paper>
   );
 };
--- src/components/wrapper_components/Input.WrapperComponent.tsx
+++ src/components/wrapper_components/Input.WrapperComponent.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { InputBase } from "@mui/material";
 
-const Input = () => {
+interface InputProps {
+  input:Function;
+}
+
+const Input: React.FC<InputProps>  = (props) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.input(event.target.value)
+  };
   return (
     <InputBase
       sx={{ width: "100%" }}
       placeholder="Explore the topics..."
+      onChange={onChange}
       inputProps={{
         sx: {
+          fontSize: "25px",
           "&::placeholder": {
             color: "#D3DFF8",
             fontFamily: "Roboto",
             fontStyle: "normal",
-            fontWeight: "400",
+            fontWeight: "600",
             fontSize: "25px",
             lineHeight: "47px",
           },
