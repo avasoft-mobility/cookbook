df3bf7df4ec429c8a81fd1f10a0fdcb7d22209ce
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-07-06 20:52:35 +0530

    Refactor the Pill component with string acceptable
    Changed the pill component with accept the color as string for custom color for pill

--- src/components/wrapper_components/Pill.WrapperComponent.tsx
+++ src/components/wrapper_components/Pill.WrapperComponent.tsx
@@ -2,18 +2,47 @@ import Chip from "@mui/material/Chip";
 
 interface PillProps {
   label: string;
-  color:
-    | "default"
-    | "primary"
-    | "secondary"
-    | "error"
-    | "info"
-    | "success"
-    | "warning";
+  color: string;
 }
 
+const VALID_PRE_DEFINED_COLORS = [
+  "default",
+  "primary",
+  "secondary",
+  "error",
+  "info",
+  "success",
+  "warning",
+];
+
+type PillColors =
+  | "default"
+  | "primary"
+  | "secondary"
+  | "error"
+  | "info"
+  | "success"
+  | "warning";
+
 const Pill: React.FC<PillProps> = (props) => {
-  return <Chip label={props.label} variant="outlined" color={props.color} />;
+  const IsHexColorCode = (colorCode: string): boolean => {
+    return !VALID_PRE_DEFINED_COLORS.includes(colorCode);
+  };
+
+  return (
+    <Chip
+      label={props.label}
+      variant="outlined"
+      color={
+        IsHexColorCode(props.color) ? "primary" : (props.color as PillColors)
+      }
+      sx={
+        IsHexColorCode(props.color)
+          ? { color: props.color, borderColor: props.color }
+          : null
+      }
+    />
+  );
 };
 
 export default Pill;
