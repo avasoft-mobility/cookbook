197490a210a8518c7e67d0629bf58dfc38e46f60
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-06 18:17:59 +0530

    Implement Kebab component

--- src/Pages/Home.page.tsx
+++ src/Pages/Home.page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "../components/wrapper_components/Header.Wrapper.component";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return <div><Header headerText="Push Notification Implementation" headerHeight="50px"  /></div>;
 };
 
 export default HomePage;
--- src/components/wrapper_components/Header.Wrapper.component.tsx
+++ src/components/wrapper_components/Header.Wrapper.component.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import { Box, IconButton } from "@mui/material";
 import Text from "./Text.wrapperComponent";
 import MoreIcon from "@mui/icons-material/MoreVert";
+import KebabMenu from "./KebabMenu.WrapperComponent";
 
 interface HeaderProps {
   headerText: string;
+  headerHeight?: string
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const onClickMenuItem =(event: React.MouseEvent<HTMLElement>)=>{
+    console.log(event.currentTarget)
+    setAnchorEl(null);
+  }
   return (
     <Box
       sx={{
@@ -17,35 +31,31 @@ const Header: React.FC<HeaderProps> = (props) => {
         justifyContent: "space-between",
         alignItems: "center",
         backgroundColor: "#6133BD",
+        height: props.headerHeight
       }}
     >
       <div style={styles.headerText}>
-        <Text variant="h6" color="#FFFFFF">
+        <Text variant="inherit" color="#FFFFFF">
           {props.headerText}
         </Text>
       </div>
       <div>
-        <IconButton
-          size="large"
-          aria-label="display more actions"
-          edge="end"
-          color="inherit"
-        >
-          <MoreIcon sx={{ color: "#FFFFFF" }} />
-        </IconButton>
+        <KebabMenu
+          iconButtonColor={"default"}
+          iconButtonStyle = {{color:"#FFFFFF"}}
+          onMenuItemClicked={onClickMenuItem}
+          onIconButtonClicked={handleClick}
+          menuItems={["Download Code","See Topic Flow","See Technical Flow"]}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        />
       </div>
     </Box>
   );
 };
 
 const styles = {
-  headerContainer: {
-    width: "100%",
-    backgroundColor: "#6133BD",
-    height: "60px",
-    display: "flex",
-    alignItems: "center",
-  },
   headerText: {
     marginLeft: "15px",
   },
