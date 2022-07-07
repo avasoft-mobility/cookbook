e892732bdb8320cb48999b549b6a6469330920b9
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-07 16:15:07 +0530

    Setup routes to Home and Details pages

--- src/configs/RouterConfig.tsx
+++ src/configs/RouterConfig.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
+import DetailsPage from "../Pages/DetailsPage.page";
 import HomePage from "../Pages/Home.page";
 
 const RouterConfig = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/topics" element={<HomePage />} />
+      <Route path="/topics/:topicSlug" element={<DetailsPage />} />
     </Routes>
   );
 };
