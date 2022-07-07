7faa85a4494bff855bca217f4df93b918ff6b799
ragulraj-avasoft <105632354+ragulraj-avasoft@users.noreply.github.com>
2022-07-07 22:27:00 +0530

    Implement Theme Color (#65)

--- src/components/wrapper_components/AccordionList.WrapperComponent.tsx
+++ src/components/wrapper_components/AccordionList.WrapperComponent.tsx
@@ -3,6 +3,8 @@ import Step from "../../models/Step.Model";
 import Accordion from "./Accordion.WrapperComponent";
 import AccordionContent from "./AccordionContent.Wrapper";
 import AccordionTitle from "./AccordionTitle.WrapperComponent";
+import Theme from '../../configs/ThemeConfig'
+import Color from '../../configs/ColorConfig'
 
 interface AccordionListProps {
   steps: Step[];
@@ -14,15 +16,15 @@ const AccordionList: React.FC<AccordionListProps> = (props) => {
       {props.steps.map((step: Step) => (
         <Accordion
           key={step.id}
-          iconColor="#648DE5"
+          iconColor={Theme.palette.secondary.main}
           accordionTitle={
-            <AccordionTitle title={step.title} id={step.id} idColor="#6133BD" />
+            <AccordionTitle title={step.title} id={step.id} idColor={Theme.palette.primary.main} />
           }
           accordionContent={
             <AccordionContent
               content={step.description}
               code={step.code}
-              contentColor="#424242"
+              contentColor={Color.texttertiaryColor}
               image={step.image}
             />
           }
