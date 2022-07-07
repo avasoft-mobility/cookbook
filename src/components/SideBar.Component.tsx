4224a75c53557f81d3ee0d6e2d06039417d0c2ec
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-07-07 18:38:16 +0530

    Chnage side bar color

--- src/components/SideBar.Component.tsx
+++ src/components/SideBar.Component.tsx
@@ -1,56 +1,84 @@
 import {
+  colors,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
+
+import Color from "../configs/ColorConfig";
+
 import Icon from "./wrapper_components/Icon.WrapperComponent";
 
+import Cookbook from "../models/Cookbook.Model";
+
 interface SideBarProps {
-  selectedTopic: string;
-  Topics: string[];
+  selectedStack: string;
+  stacks: Cookbook[];
   onSelect: Function;
 }
 
-const SideBar: React.FC<SideBarProps> = (props) => {
+const SideBar: React.FC<SideBarProps> = ({
+  stacks,
+  onSelect,
+  selectedStack,
+}) => {
   const getIcon = (stackName: string) => {
     if (stackName === "React Native") {
-      return <Icon type="reactNative" style={{ width: 30, height: 30 }} />;
+      return <Icon type="reactNative" style={styles.logo} />;
     }
 
     if (stackName === "Native Android") {
-      return <Icon type="androidStudio" style={{ width: 30, height: 30 }} />;
+      return <Icon type="androidStudio" style={styles.logo} />;
     }
 
     if (stackName === "Native Ios") {
-      return <Icon type="swift" style={{ width: 30, height: 30 }} />;
+      return <Icon type="swift" style={styles.logo} />;
     }
 
     if (stackName === "Flutter") {
-      return <Icon type="flutter" style={{ width: 30, height: 30 }} />;
+      return <Icon type="flutter" style={styles.logo} />;
     }
 
     if (stackName === "Xamarin Forms") {
-      return <Icon type="xamarin" style={{ width: 30, height: 30 }} />;
+      return <Icon type="xamarin" style={styles.logo} />;
     }
   };
 
   return (
     <List>
-      {props.Topics.map((stackName) => (
-        <ListItem key={stackName} disablePadding>
+      {stacks.map((cookbook: Cookbook) => (
+        <ListItem key={cookbook.stack.id} disablePadding>
           <ListItemButton
-            selected={stackName === props.selectedTopic ? true : false}
-            onClick={() => props.onSelect(stackName)}
+            sx={{
+              backgroundColor:
+                cookbook.stack.name === selectedStack
+                  ? Color.primaryTintColor
+                  : null,
+              "&:hover": {
+                backgroundColor: Color.primaryLight,
+              },
+            }}
+            onClick={() => onSelect(cookbook.stack.name)}
           >
-            <ListItemIcon>{getIcon(stackName)}</ListItemIcon>
-            <ListItemText primary={stackName} style={{ color: "#000000" }} />
+            <ListItemIcon>{getIcon(cookbook.stack.name)}</ListItemIcon>
+            <ListItemText
+              primary={cookbook.stack.name}
+              style={{ color: Color.textSecondaryColor }}
+            />
           </ListItemButton>
         </ListItem>
       ))}
     </List>
   );
+};
+
+const styles = {
+  logo: {
+    width: 26,
+    height: 26,
+  },
 };
 
 export default SideBar;
