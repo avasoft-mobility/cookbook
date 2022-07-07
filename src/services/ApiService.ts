8d6c7ae5434ed3cfa83e0f49fc0a46b4d906ef28
clement-avasoft <105507541+clement-avasoft@users.noreply.github.com>
2022-07-07 15:35:17 +0530

    Add fetchTopics and fetchTopic methods (#46)

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,7 +1,26 @@
-class ApiService {
-  public static async fetchTopics() {}
+import DummyData from "../dummy-data.json";
+import Topic from "../models/Topic.Model";
 
-  public static async fetchDetails(topic: string) {}
+class ApiService {
+  public static fetchTopics = (topic?: string) => {
+    let topics: Topic[] = JSON.parse(JSON.stringify(DummyData));
+    let searchedTopics: Topic[] = [];
+    if (topic !== undefined) {
+      topics.forEach((element) => {
+        if (element.title.includes(topic)) {
+          searchedTopics.push(element);
+        }
+      });
+      return searchedTopics;
+    }
+
+    return topics;
+  };
+
+  public static fetchTopic = (id: string) => {
+    let topics: Topic[] = JSON.parse(JSON.stringify(DummyData));
+    return topics[0];
+  };
 }
 
 export default ApiService;
