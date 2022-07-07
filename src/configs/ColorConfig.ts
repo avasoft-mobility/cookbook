a0b90df9f4acd92bf7b4266be0cec81bfb89873c
Jiju-avasoft <105510110+jiju-avasoft@users.noreply.github.com>
2022-07-07 19:20:00 +0530

    Add SideBar color and hover color (#54)

--- src/configs/ColorConfig.ts
+++ src/configs/ColorConfig.ts
@@ -1,11 +1,12 @@
 class Color {
   static primaryColor: string = "#6133BD";
-  static primaryTintColor: string = "#D9D9D9";
+  static primaryTintColor: string = "#E1D7F4";
   static secondaryColor: string = "#648DE5";
   static secondaryTintColor: string = "#D3DFF8";
   static textPrimaryColor: string = "#FFFFFF";
   static textSecondaryColor: string = "#000000";
   static texttertiaryColor: string = "#424242";
   static tagColor: string = "#F9C0C0";
+  static primaryLight: string = "#E1D7F455";
 }
 export default Color;
