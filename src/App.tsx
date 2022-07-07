aaa13ab4bd4a40bc5520927713517c58509edf93
ragulraj-avasoft <105632354+ragulraj-avasoft@users.noreply.github.com>
2022-07-07 18:03:15 +0530

    Config Theme and Color (#50)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { ThemeProvider } from "@emotion/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import RouterConfig from "./configs/RouterConfig";
 
+import Theme from "./configs/ThemeConfig";
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <RouterConfig />
+        <ThemeProvider theme={Theme}>
+          <RouterConfig />
+        </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
   );
---
+++ src/configs/ColorConfig.ts
@@ -0,0 +1,11 @@
+class Color {
+  static primaryColor: string = "#6133BD";
+  static primaryTintColor: string = "#D9D9D9";
+  static secondaryColor: string = "#648DE5";
+  static secondaryTintColor: string = "#D3DFF8";
+  static textPrimaryColor: string = "#FFFFFF";
+  static textSecondaryColor: string = "#000000";
+  static texttertiaryColor: string = "#424242";
+  static tagColor: string = "#F9C0C0";
+}
+export default Color;
