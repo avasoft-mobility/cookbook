63c0557cdb47b39d342df8f3a2193288d9ee3ed7
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-08 16:11:47 +0530

    Modify from directly load home page

--- src/configs/RouterConfig.tsx
+++ src/configs/RouterConfig.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import DetailsPage from "../Pages/DetailsPage.page";
 import HomePage from "../Pages/Home.page";
 
 const RouterConfig = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Navigate to="/topics" />} />
       <Route path="/topics" element={<HomePage />} />
       <Route path="/topics/:topicSlug" element={<DetailsPage />} />
     </Routes>
