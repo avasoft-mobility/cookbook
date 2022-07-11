1dbe2023ce32ca9a344fb5dcb3f35f1c73f1c8a8
ragulraj-avasoft <105632354+ragulraj-avasoft@users.noreply.github.com>
2022-07-11 20:22:02 +0530

    Implement ConfirmationDialog Wrapper (#82)

---
+++ src/components/wrapper_components/ConfirmationDialog.WrapperComponent.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ConfirmationDialogRow from "./ConfirmationDialogRow.WrapperComponent";
+
+interface ConfirmationDialogProps {
+  title: string;
+  dialogValues: string[];
+  dialogHeader: string;
+  onConfirm: Function;
+}
+
+const ConfirmationDialog: React.FC<ConfirmationDialogProps> = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("Choose the Topic");
+
+  const openDialog = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (newValue?: string) => {
+    setOpen(false);
+    if (newValue) {
+      setValue(newValue);
+    }
+    props.onConfirm(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <List component="div" role="group">
+        <ListItem button divider aria-haspopup="true" onClick={openDialog}>
+          <ListItemText primary={props.title} secondary={value} />
+        </ListItem>
+        <ConfirmationDialogRow
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          value={value}
+          dialogValues={props.dialogValues}
+          DialogHeader={props.dialogHeader}
+        />
+      </List>
+    </Box>
+  );
+};
+
+export default ConfirmationDialog;
