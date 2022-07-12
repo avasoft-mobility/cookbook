47183036dd1316793b63771a20dd74c107faf257
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-12 20:46:11 +0530

    Implement add new stack page

--- src/configs/RouterConfig.tsx
+++ src/configs/RouterConfig.tsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { CookbookPage } from "../Pages/CookbookPage.page";
 import DetailsPage from "../Pages/DetailsPage.page";
 import HomePage from "../Pages/Home.page";
+import Stackpage from "../Pages/Stackpage.page";
 
 const RouterConfig = () => {
   return (
@@ -8,6 +10,8 @@ const RouterConfig = () => {
       <Route path="/" element={<Navigate to="/topics" />} />
       <Route path="/topics" element={<HomePage />} />
       <Route path="/topics/:topicSlug" element={<DetailsPage />} />
+      <Route path="/create/stack" element={<Stackpage />} />
+      <Route path="/create/cookbook" element={<CookbookPage />} />
     </Routes>
   );
 };
