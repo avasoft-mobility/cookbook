916c1b39ec5df632d8cc3d0b9971dac268b6d100
Clement   Ambrose <Clement.A@avasoft.com>
2022-07-13 20:37:46 +0530

    Create separate component for main content home page

--- src/Pages/Home.page.tsx
+++ src/Pages/Home.page.tsx
@@ -1,4 +1,3 @@
-import { Grid } from "@mui/material";
 import { useState } from "react";
 import { useDebounce } from "use-debounce";
 
@@ -7,14 +6,13 @@ import { useNavigate } from "react-router-dom";
 import ApiService from "../services/ApiService";
 
 import Search from "../components/Search.component";
-import TopicItem from "../components/TopicItem.Component";
 import Text from "../components/wrapper_components/Text.wrapperComponent";
 
 import { useQuery } from "react-query";
 import "../App.css";
+import HomePageMainContent from "../components/HomePageMainContent.component";
 import Theme from "../configs/ThemeConfig";
 import Topic from "../models/Topic.Model";
-import SkeletonLoader from "../components/SkeletonLoader.Component";
 
 const HomePage = () => {
   let [topics, setTopic] = useState<Topic[]>([]);
@@ -41,50 +39,6 @@ const HomePage = () => {
     navigate(`/topics/${slug}`);
   };
 
-  const loadTopics = () => {
-    if (isLoading) {
-      return (
-        <div style={styles.innerContainer}>
-          <SkeletonLoader />
-        </div>
-      );
-    }
-
-    if (topics.length === 0) {
-      return (
-        <div style={styles.innerContainer}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text variant={"h6"} color={""}>
-              No Topics Found
-            </Text>
-          </div>
-        </div>
-      );
-    }
-
-    return (
-      <div style={styles.innerContainer}>
-        {topics !== undefined ? (
-          <Grid container spacing={10}>
-            {topics.map((topic: Topic) => {
-              return (
-                <Grid key={topic._id} item xs={6}>
-                  <TopicItem topic={topic} onSelect={onSelect} />
-                </Grid>
-              );
-            })}
-          </Grid>
-        ) : null}
-      </div>
-    );
-  };
-
   return (
     <div style={styles.container}>
       <div className="innerContainer">
@@ -95,7 +49,11 @@ const HomePage = () => {
         </div>
         <Search input={onSearch} />
       </div>
-      {loadTopics()}
+      <HomePageMainContent
+        isLoading={isLoading}
+        topics={topics}
+        onSelect={onSelect}
+      />
     </div>
   );
 };
