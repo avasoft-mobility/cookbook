bbfaf1d9aa8726669267356dbc3394cf7c422742
navayuvan-avasoft <navayuvan.s@avasoft.com>
2022-07-14 21:47:00 +0530

    Change Pages directory name to all-small

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+
+import { useNavigate } from "react-router-dom";
+
+import ApiService from "../services/ApiService";
+
+import Search from "../components/specified_components/home_page_components/Search.component";
+import Text from "../components/wrapper_components/Text.wrapperComponent";
+
+import { useQuery } from "react-query";
+import { AxiosError } from "axios";
+import "../App.css";
+import HomePageMainContent from "../components/specified_components/home_page_components/HomePageMainContent.component";
+import Theme from "../configs/ThemeConfig";
+import Topic from "../models/Topic.Model";
+import ErrorResponse from "../models/request_response_models/Error.Response.model";
+import useErrorSnackbar from "../hooks/useErrorSnackbar.hook";
+
+const HomePage = () => {
+  const showErrorSnackBar = useErrorSnackbar();
+
+  let [topics, setTopic] = useState<Topic[]>([]);
+  let [searchText, setSearchText] = useState<string>("");
+
+  const debouncedSearchText = useDebounce(searchText, 500);
+  const { isLoading } = useQuery(
+    ["topics", debouncedSearchText],
+    () => ApiService.fetchTopics(searchText),
+    {
+      onSuccess: (data) => {
+        setTopic(data);
+      },
+      onError: (error: AxiosError) => {
+        showErrorSnackBar((error.response?.data as ErrorResponse).message);
+      },
+    }
+  );
+
+  let navigate = useNavigate();
+
+  const onSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  const onSelect = (slug: string) => {
+    navigate(`/topics/${slug}`);
+  };
+
+  return (
+    <div style={styles.container}>
+      <div className="innerContainer">
+        <div style={{ marginBottom: "30px" }}>
+          <Text variant="h3" color={Theme.palette.primary.main}>
+            CookBook
+          </Text>
+        </div>
+        <Search input={onSearch} />
+      </div>
+      <HomePageMainContent
+        isLoading={isLoading}
+        topics={topics}
+        onSelect={onSelect}
+      />
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    width: "100%",
+    height: "100vh",
+  },
+  innerContainer: {
+    flex: 1,
+    padding: "30px 20%",
+  },
+};
+
+export default HomePage;
---
+++ src/pages/create_pages/CreateTag.page.tsx
@@ -0,0 +1,206 @@
+import { useState } from "react";
+import { AxiosError } from "axios";
+
+import { useNavigate } from "react-router-dom";
+
+import CircularProgress from "@mui/material/CircularProgress";
+
+import { useMutation } from "react-query";
+
+import Theme from "../../configs/ThemeConfig";
+
+import ApiService from "../../services/ApiService";
+
+import Clickable from "../../components/wrapper_components/ButtonWrapperComponent";
+import Input from "../../components/wrapper_components/Input.WrapperComponent";
+import Text from "../../components/wrapper_components/Text.wrapperComponent";
+import Color from "../../configs/ColorConfig";
+
+import useErrorSnackbar from "../../hooks/useErrorSnackbar.hook";
+import ErrorResponse from "../../models/request_response_models/Error.Response.model";
+
+interface TagPageData {
+  tagName: string;
+  errors: string;
+}
+
+const CreateTagPage = () => {
+  const showErrorSnackBar = useErrorSnackbar();
+
+  const [tagData, setTagData] = useState<TagPageData>();
+  const navigate = useNavigate();
+
+  const mutation = useMutation(ApiService.addTag, {
+    onSuccess: () => {
+      navigate("/create/topic");
+    },
+    onError: (error: AxiosError) => {
+      showErrorSnackBar((error.response?.data as ErrorResponse).message);
+    },
+  });
+
+  const validateStackName = (value?: string) => {
+    if (value === undefined) {
+      setTagData({
+        tagName: "",
+        errors: "Tag name required",
+      });
+      return false;
+    }
+
+    if (value === "") {
+      setTagData({
+        tagName: "",
+        errors: "Tag name required",
+      });
+      return false;
+    }
+
+    if (value!.length < 2) {
+      setTagData({
+        tagName: tagData!.tagName,
+        errors: "Tag name length should be atleast 2 characters",
+      });
+      return false;
+    }
+
+    return true;
+  };
+
+  const onInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setTagData({ tagName: event.target.value, errors: "" });
+  };
+
+  const onInputBlur = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    validateStackName(event.target.value);
+  };
+
+  const onAddButtonClicked = () => {
+    if (validateStackName(tagData?.tagName)) {
+      mutation.mutate(tagData!.tagName);
+    }
+  };
+
+  const onAddNewTopicClicked = () => {
+    navigate("/create/topic");
+  };
+
+  return (
+    <div style={{ ...style.mainContainer, ...{ flexDirection: "column" } }}>
+      <div style={style.headingContainer}>
+        <Text variant={"h4"} color={Theme.palette.primary.main}>
+          Create Tag
+        </Text>
+      </div>
+
+      <div style={{ ...style.subContainer, ...{ flexDirection: "column" } }}>
+        <div style={style.bodyContainer}>
+          <Text variant={"body2"} color={""}>
+            Enter tag name
+          </Text>
+          <div style={style.inputContainer}>
+            <Input
+              onChange={(event) => {
+                onInputChange(event);
+              }}
+              onBlur={onInputBlur}
+              inputTextStyle={{
+                width: "100%",
+                fontSize: "16px",
+                padding: "12px 8px",
+                color: Theme.palette.text.secondary,
+              }}
+            />
+          </div>
+
+          <Text variant={"inherit"} color={"#DB4437"}>
+            {tagData?.errors}
+          </Text>
+        </div>
+
+        <div style={style.buttonContainer}>
+          {!mutation.isLoading ? (
+            <>
+              <div>
+                <Clickable
+                  ClickableText={"Add Tag"}
+                  variant={"contained"}
+                  clickableSize={"large"}
+                  onClick={onAddButtonClicked}
+                  style={style.addButton}
+                />
+              </div>
+              <div>
+                <Clickable
+                  ClickableText={"ADD NEW TOPIC"}
+                  variant={"text"}
+                  textColor={Color.lightTextSecondaryColor}
+                  clickableSize={"large"}
+                  onClick={onAddNewTopicClicked}
+                  style={style.newTopicButton}
+                />
+              </div>
+            </>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const style = {
+  mainContainer: {
+    display: "flex",
+    flex: 1,
+    padding: "30px 20%",
+    height: "100vh",
+    justifyContent: "space-between",
+  },
+  headingContainer: {
+    flex: 1,
+  },
+  subContainer: {
+    display: "flex",
+    flex: 7,
+    justifyContent: "space-between",
+  },
+  bodyContainer: {
+    flex: 2,
+  },
+  inputContainer: {
+    border: "1px solid #6133BD",
+    width: "100%",
+    marginBottom: "30px",
+    marginTop: "10px",
+    paddingLeft: "10px",
+    borderRadius: "10px",
+  },
+  buttonContainer: {
+    flex: 4,
+    marginBottom: "40px",
+  },
+  addButton: {
+    width: "100%",
+    marginBottom: "20px",
+  },
+  newTopicButton: {
+    width: "100%",
+    fontWeight: "400",
+  },
+};
+
+export default CreateTagPage;
