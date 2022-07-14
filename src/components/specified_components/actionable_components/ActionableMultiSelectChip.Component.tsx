effad5b0eb5357033ce2b9ef91cac0c62bb87d59
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-07-14 22:01:00 +0530

    Create Actionable multiselect chip

---
+++ src/components/specified_components/actionable_components/ActionableMultiSelectChip.Component.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import Text from "../..//wrapper_components/Text.wrapperComponent";
+import Clickable from "../..//wrapper_components/ButtonWrapperComponent";
+import MultipleSelectChip from "../../wrapper_components/MultipleSelectChip.WrapperComponent";
+
+import Color from "../../../configs/ColorConfig";
+import Theme from "../../../configs/ThemeConfig";
+
+interface ActionableMultiSelectChipProps {
+  tags: string[];
+  onTagChange: Function;
+  createNewTag: Function;
+  title: string;
+  errorMessage?: string;
+}
+
+const ActionableMultiSelectChip: React.FC<ActionableMultiSelectChipProps> = (
+  props
+) => {
+  return (
+    <div>
+      <Text variant="body2" color={Theme.palette.text.secondary}>
+        {props.title}
+      </Text>
+      <div style={styles.alignItemsCenter as React.CSSProperties}>
+        <div style={styles.multipleSelectContainer}>
+          <MultipleSelectChip
+            inputLabel="Tags"
+            values={props.tags}
+            chipStyle={{ color: Theme.palette.primary.main }}
+            menuItemStyle={{ color: Theme.palette.secondary.main }}
+            onChange={(event) =>
+              props.onTagChange(event.target.value as string[])
+            }
+          />
+        </div>
+        <Clickable
+          ClickableText={"Add Tag"}
+          variant={"text"}
+          clickableSize={"large"}
+          onClick={() => props.createNewTag()}
+          textColor={Color.primaryColor}
+          style={styles.addButton}
+        />
+      </div>
+      {props.errorMessage !== undefined && props.errorMessage !== "" ? (
+        <Text color={Color.errorMessage} variant="body2">
+          {props.errorMessage}
+        </Text>
+      ) : null}
+    </div>
+  );
+};
+
+const styles = {
+  alignItemsCenter: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: "10px",
+  },
+  multipleSelectContainer: {
+    width: "100%",
+    marginRight: "20px",
+  },
+  addButton: {
+    width: "150px",
+    height: "50px",
+  },
+};
+
+export default ActionableMultiSelectChip;
