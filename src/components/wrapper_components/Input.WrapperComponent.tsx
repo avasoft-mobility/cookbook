d39038c1057af45daa642684588d4bcfe1c5ac3b
Ragul Raj Rajkumar <Ragul.RA@avasoft.com>
2022-07-15 15:39:01 +0530

    Made the return statement specifically for each type of input

--- src/components/wrapper_components/Input.WrapperComponent.tsx
+++ src/components/wrapper_components/Input.WrapperComponent.tsx
@@ -1,12 +1,14 @@
+import React from "react";
 import {
   createTheme,
   InputBase,
   TextField,
   ThemeProvider,
 } from "@mui/material";
-import React from "react";
+
 import Color from "../../configs/ColorConfig";
 import Theme from "../../configs/ThemeConfig";
+
 interface InputProps {
   onChange:
     | React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
@@ -21,6 +23,7 @@ interface InputProps {
   errorText?: string;
   style?: React.CSSProperties;
 }
+
 const Input: React.FC<InputProps> = (props) => {
   const theme = createTheme({
     palette: {
@@ -30,52 +33,58 @@ const Input: React.FC<InputProps> = (props) => {
       },
     },
   });
-  return (
-    <>
-      {props.type === "outlined" ? (
-        <ThemeProvider theme={theme}>
-          <TextField
-            error={props.errorText !== "" ? true : false}
-            id="outlined-basic"
-            fullWidth
-            label={props.errorText === "" ? props.placeHolderText : "error"}
-            helperText={props.errorText !== "" ? props.errorText : null}
-            variant="outlined"
-            onChange={props.onChange}
-            onBlur={props.onBlur}
-            value={props.value}
-            multiline
-            sx={props.style}
-            rows={props.numberOfLines}
-          />
-        </ThemeProvider>
-      ) : (
-        <InputBase
-          sx={
-            props.inputTextStyle !== undefined
-              ? props.inputTextStyle
-              : {
-                  width: "100%",
-                  fontSize: "25px",
-                  color: Theme.palette.text.secondary,
-                }
-          }
-          placeholder={props.placeHolderText}
+
+  const outlinedInputTextField = () => {
+    return (
+      <ThemeProvider theme={theme}>
+        <TextField
+          error={props.errorText !== "" ? true : false}
+          id="outlined-basic"
+          fullWidth
+          label={props.errorText === "" ? props.placeHolderText : "error"}
+          helperText={props.errorText !== "" ? props.errorText : null}
+          variant="outlined"
           onChange={props.onChange}
-          rows={props.numberOfLines}
-          inputProps={{
-            sx: {
-              "&::placeholder":
-                props.placeholderStyle !== undefined && props.placeholderStyle,
-            },
-          }}
-          multiline
-          value={props.value}
           onBlur={props.onBlur}
+          value={props.value}
+          multiline
+          sx={props.style}
+          rows={props.numberOfLines}
         />
-      )}
-    </>
+      </ThemeProvider>
+    );
+  };
+
+  if (props.type === "outlined") {
+    return outlinedInputTextField();
+  }
+
+  return (
+    <InputBase
+      sx={
+        props.inputTextStyle !== undefined
+          ? props.inputTextStyle
+          : {
+              width: "100%",
+              fontSize: "25px",
+              color: Theme.palette.text.secondary,
+            }
+      }
+      placeholder={props.placeHolderText}
+      onChange={props.onChange}
+      rows={props.numberOfLines}
+      inputProps={{
+        sx: {
+          "&::placeholder":
+            props.placeholderStyle !== undefined && props.placeholderStyle,
+        },
+      }}
+      multiline
+      value={props.value}
+      onBlur={props.onBlur}
+    />
   );
 };
+
 export default Input;
 export type { InputProps };
